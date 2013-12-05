e357aeb0a84e31576af4f50cf910a238a8822584
seanvoss <seanvoss@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-05 05:23:43 +0000

    Cutting 0.18
    git-svn-id: https://plugins.svn.wordpress.org/striper/trunk@815253 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: seanvoss
 Tags: woocommerce, stripe, payments, free stripe integration
 Requires at least: 3.0
 Tested up to: 3.6
-Stable tag: 0.17
+Stable tag: 0.18
 Donate link: https://blog.seanvoss.com/shop/striper/
 License: GPLv2 or later
 
@@ -31,6 +31,8 @@ None at this time
 
 == Changelog ==
 
+= 0.18 =
+* Fixes intermittant infinite loop, and fixes the image submit button
 = 0.17 =
 * Drastic change in how the JS code executes
 = 0.16 =
--- striper.js
+++ striper.js
@@ -1,50 +1,35 @@
   var initStriper = function(){
     jQuery(function($) {
-    var $form = $('form.checkout,form#order_review');
-        
-    var deferred = $.Deferred();
-    deferred.fail(function(){
-    
-    });
+      var $form = $('form.checkout,form#order_review');
+          
+      var deferred = $.Deferred();
 
-    $('body').on('click', 'form#order_review input:submit', function(){
-      // Make sure there's not an old token on the form
-      $('form#order_review').find('[name=stripeToken]').remove()
-      Stripe.setPublishableKey($('#stripe_pub_key').data('publishablekey'));
-      Stripe.createToken($form, stripeResponseHandler);
-      return false;
-    });
+      var delayForToken = function($form){
+          Stripe.card.createToken($form, stripeResponseHandler);
+          return deferred.promise();
+      }
+      var stripeResponseHandler = function(status, response) {
+      $('form.checkout').find('[name=stripeToken]').remove()
 
-    var delayForToken = function($form){
-        Stripe.card.createToken($form, stripeResponseHandler);
-        return deferred.promise();
-    }
-    var stripeResponseHandler = function(status, response) {
-    $('form.checkout').find('[name=stripeToken]').remove()
+      if (response.error) {
 
-    if (response.error) {
+        // Show the errors on the form
+        $form.find('.payment-errors').css({'color': 'red'}).text(response.error.message);
 
-      // Show the errors on the form
-      $form.find('.payment-errors').css({'color': 'red'}).text(response.error.message);
-      // Unblock the form to re-enter data
-      $form.unblock();
+        deferred.fail();
+      } else {
+        // Append the Token
+        $form.append($('<input type="hidden" name="stripeToken" />').val(response.id));
 
-      deferred.fail();
-    } else {
-      // Append the Token
-      $form.append($('<input type="hidden" name="stripeToken" />').val(response.id));
+        deferred.resolve();
+      }
+    };
+    $('body').on('click', 'form.checkout input#place_order', function(e){
 
-      deferred.resolve();
-    }
-  };
-
-    // Bind to the checkout_submit event to add the token
-    $('body').on('click', 'form.checkout input:submit', function(e){
-
+      var button = $(e.currentTarget);
       $form = $('form.checkout');
-      window.striper = true;
       if($('input[name=payment_method]:checked').val() != 'Striper'){
-         $form.submit();
+          return true;
       }
 
       $form.find('.payment-errors').html('');
@@ -52,18 +37,17 @@
       // Pass if we have a token
       if( $form.find('[name=stripeToken]').length)
       {
-        $form.submit();
         return true;
       }
       e.preventDefault();
 
       Stripe.setPublishableKey($('#stripe_pub_key').data('publishablekey'));
-      return $.when(delayForToken($form)).then($.proxy(function(){
-       $form.submit();
-        return true;
-      },this), $.proxy(function(){
+      return delayForToken($form).then(function(){
+        button.click();
         return false;
-      },this));
+      }, function(){
+        return false;
+      });
     });
   });
 };
