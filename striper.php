12fe193184c86fc6cc8589a76a7a1a7a39beafed
seanvoss <seanvoss@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-12 19:31:46 +0000

    Bump Version
    git-svn-id: https://plugins.svn.wordpress.org/striper/trunk@1179833 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- striper.php
+++ striper.php
@@ -3,7 +3,7 @@
 Plugin Name: Striper (Gateway using Stripe.js)
 Plugin URI: http://blog.seanvoss.com/product/striper
 Description: Provides a Credit Card Payment Gateway through Stripe for woo-commerece.
-Version: 0.30
+Version: 0.91
 Author: Sean Voss
 Author URI: https://blog.seanvoss.com/
 
