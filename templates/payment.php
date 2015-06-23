2c67b9a409fb1c1e7b18eabd3a98cdf34dd8075a
seanvoss <seanvoss@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-23 21:22:00 +0000

    Bump Version
    git-svn-id: https://plugins.svn.wordpress.org/striper/trunk@1186474 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- templates/payment.php
+++ templates/payment.php
@@ -7,7 +7,6 @@
  */
 ?>
 
-<div id="stripe_pub_key" class="hidden" style="display:none" data-publishablekey="<?=$this->publishable_key ?>"> </div>
 <div class="clear"></div>
 <span class='payment-errors required'></span>
 <p class="form-row">
@@ -54,42 +53,13 @@
 </p>
 <div class="clear"></div>
 
-<script>
+<script type="text/javascript" src="https://js.stripe.com/v2/"></script>
+<script type="text/javascript">
 
-  var initStriper = function(){
+ var initStriper = function(){
     jQuery(function($) {
+
     var $form = $('form.checkout,form#order_review');
-
-    // Add additional information to be passed to Stripe
-    var stripeMap = {
-
-        billing_address_1:  'address_line1',
-        billing_address_2:  'address_line2',
-        billing_city:       'address_city',
-        billing_country:    'address_country',
-        billing_state:      'address_state',
-        billing_postcode:   'address_zip',
-    }
-    var card_name = '';
-    $('form.checkout').find('input[id*=billing_],select[id*=billing_]').each(function(idx,el){
-        var mapped = stripeMap[el.id];
-        if (mapped)
-        {
-            $(el).attr('data-stripe',mapped);
-            
-        }
-        if(el.id == 'billing_first_name' || el.id == 'billing_last_name')
-        {
-            card_name += $(el).val() + ' ';
-        }
-        
-        
-    });
-    if (!$('#stripeCardName').length)
-    {
-        $('<input id="stripeCardName" class="input-text" type="hidden" data-stripe="name" value="'+card_name+'"/>').appendTo($form);
-    }
-
     var stripeResponseHandler = function(status, response) {
 
     if (response.error) {
@@ -109,36 +79,30 @@
     }
   };
 
-    $('body').on('click', '#place_order,form#order_review input:submit', function(){
-      if(jQuery('.payment_methods input:checked').val() !== 'Striper')
-      {
-        return true;
-      }
+    $('body').on('click', 'form#order_review input:submit', function(){
 
+       if($('input[name=payment_method]:checked').val() != 'Striper'){
+           return true;
+       }
       // Make sure there's not an old token on the form
-      Stripe.setPublishableKey($('#stripe_pub_key').data('publishablekey'));
+      Stripe.setPublishableKey('<?php echo $this->publishable_key ?>');
       Stripe.createToken($form, stripeResponseHandler);
       return false;
     });
 
 
-    $('body').on('click', '#place_order,form.checkout input:submit', function(){
-      if(jQuery('.payment_methods input:checked').val() !== 'Striper')
-      {
-        return true;
-      }
+    $('body').on('click', 'form.checkout input:submit', function(){
       // Make sure there's not an old token on the form
       $('form.checkout').find('[name=stripeToken]').remove()
     })
 
 
     // Bind to the checkout_place_order event to add the token
-    $('form.checkout').bind('#place_order,checkout_place_order_Striper', function(e){
+    $('form.checkout').bind('checkout_place_order', function(e){
 
-      if($('input[name=payment_method]:checked').val() != 'Striper'){
-          return true;
-      }
-
+     if($('input[name=payment_method]:checked').val() != 'Striper'){
+         return true;
+     }
       $form.find('.payment-errors').html('');
       $form.block({message: null,overlayCSS: {background: "#fff url(" + woocommerce_params.ajax_loader_url + ") no-repeat center",backgroundSize: "16px 16px",opacity: .6}});
 
@@ -146,7 +110,7 @@
       if( $form.find('[name=stripeToken]').length)
         return true;
 
-      Stripe.setPublishableKey($('#stripe_pub_key').data('publishablekey'));
+      Stripe.setPublishableKey('<?php echo $this->publishable_key ?>');
       Stripe.createToken($form, stripeResponseHandler)
       // Prevent the form from submitting with the default action
       return false;
@@ -165,4 +129,6 @@ if(typeof jQuery=='undefined')
 } else {
    initStriper()
 }
+
 </script>
+
